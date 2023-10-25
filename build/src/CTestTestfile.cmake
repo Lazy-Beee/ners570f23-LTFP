c17dd37c682eeafd45b8f67a0af2ff27a618ea35
Jun Fan <85976834+Lazy-Beee@users.noreply.github.com>
2023-10-25 19:05:37 -0400

    Delete build directory

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/junfan/ENGR570/LAAM_Themal_Prediction/src
-# Build directory: /home/junfan/ENGR570/LAAM_Themal_Prediction/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/junfan/ENGR570/LAAM_Themal_Prediction/tests
-# Build directory: /home/junfan/ENGR570/LAAM_Themal_Prediction/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
