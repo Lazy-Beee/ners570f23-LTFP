4060c3883e34eb381eb7b7dae388c505cfbbd63d
Lazy-Beee <junfan2017@gmail.com>
2023-10-26 01:37:04 -0400

    Update gnu_warning_flags and add documentation to ExampleClass

--- src/exampleSingleton.hpp
+++ src/exampleSingleton.hpp
@@ -6,6 +6,17 @@
 
 namespace LTFP
 {
+	/*
+	How to access this singleton class:
+	- Include header file: #include "ExampleClass.hpp"
+	- Get the pointer to the class object: ExampleClass* ec = ExampleClass::getCurrent()
+	- Or call the class function in one line: ExampleClass::getCurrent()->step()
+	Usage of accessor functions:
+	- get with const: const int var = ec->getVar(i)
+	- get with &: const int& var = ec->getVar(i) OR int& var = ec->getVar(i)
+	- set: ec->setVar(i, value)
+	*/
+
 	class ExampleClass
 	{
 	private:
@@ -22,7 +33,7 @@ namespace LTFP
 
 		static ExampleClass* getCurrent();
         
-		inline Real getExampleVariable(int i) const { return exampleVariable[i]; }
+		inline const Real getExampleVariable(int i) const { return exampleVariable[i]; }
         inline Real& getExampleVariable(int i) { return exampleVariable[i]; }
         inline void setExampleVariable(int i, Real val) { exampleVariable[i] = val; }
 	};
