b0e27910aa00b09cd064fa783157c51a27dea286
lazybee <junfan2017@gmail.com>
2023-11-03 17:21:51 -0400

    minor changes

--- simulator/default_simulator.cpp
+++ simulator/default_simulator.cpp
@@ -4,18 +4,20 @@
 
 void setSceneFile(std::string filename)
 {
-    std::cout << "Using " << filename << std::endl;
+    std::cout << "Using scene file " << filename << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
+    // Load command line inputs
     if (argc == 1)
         setSceneFile("../scenes/default.json");
     else if (argc == 2)
         setSceneFile(argv[1]);
     else
         std::cerr << "Invalid number of command line inputs" << std::endl;
-
+    
+    // Check type of Real
 #ifdef USE_DOUBLE
     std::cout << "LTPF running in double precision." << std::endl;
 #else
