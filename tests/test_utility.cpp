5c01caf3453b38c24b889dddd38988d6deaaed60
Jun Fan <junfan2017@gmail.com>
2023-11-11 00:10:06 -0500

    set CXX compiler on greatlakes

--- tests/test_utility.cpp
+++ tests/test_utility.cpp
@@ -6,35 +6,22 @@
 #include "utilities/Logger.hpp"
 #include "utilities/Timing.hpp"
 
-#include <iostream>
-#include <filesystem>
-
 using namespace std;
 using namespace LTFP;
 
 int main()
 {
-    // try
-    // {
-    //     std::cout << std::filesystem::canonical("/proc/self/exe") << std::endl;
-    // }
-    // catch(const std::filesystem::filesystem_error& e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
-    
-    
     Simulator *sim = Simulator::getCurrent();
-    // sim->initUtilities("blank.json");
+    sim->initUtilities("blank.json");
 
-    // for (int i = 0; i < 100; i++)
-    // {
-    //     START_TIMING("TEST1");
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    //     STOP_TIMING_AVG;
+    for (int i = 0; i < 100; i++)
+    {
+        START_TIMING("TEST1");
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        STOP_TIMING_AVG;
 
-    //     INCREASE_COUNTER("Test2", 0.1 * i);
-    // }
+        INCREASE_COUNTER("Test2", 0.1 * i);
+    }
 
-    // sim->finalize();
+    sim->finalize();
 }
